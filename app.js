d85338541bc633782da82d4b7e183090761d6f9c
Artem Beydin <artbeidin.exceedteam@gmail.com>
2021-11-16 17:37:57 +0300

    creacte schema. add post route

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const Schema = mongoose.Schema;
+
+const userScheme = new Schema({
+  login: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const url = 'mongodb+srv://ArtemBeydin:Restart987@cluster0.cm9vp.mongodb.net/Hospital?retryWrites=true&w=majority';
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const User = mongoose.model('users', userScheme);
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/createUser', (req, res) => {
+  const user = new User(req.body);
+  user.save().then(result => {
+    res.send({ data: result });
+  });
+});
+
+app.listen(8000, () => {
+  console.log('Hospital works on port 8000!')
+});
