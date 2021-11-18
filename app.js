0415ec662dc64fadb1c916bf82124525c6ab3225
Artem Beydin <artbeidin.exceedteam@gmail.com>
2021-11-18 14:01:27 +0300

    add env file. fix code style

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const Schema = mongoose.Schema;
-
+const { secret } = require('./config.env');
 const userScheme = new Schema({
   login: {
     type: String,
@@ -30,7 +30,7 @@ const addToken = (id) => {
   const payload = {
     id
   }
-  return jwt.sign(payload, 'key', { expiresIn: "24h" })
+  return jwt.sign(payload, secret, { expiresIn: "24h" })
 }
 
 app.post('/authorize', (req, res) => {
@@ -41,7 +41,7 @@ app.post('/authorize', (req, res) => {
         if (validPass) {
           const token = addToken(result._id);
           res.send(token);
-        } else res.status(404).send('invalid pass');        
+        } else res.status(404).send('invalid pass');
       } else res.status(404).send('user not found');
     });
   } else res.status(422).send('invalid property name');
@@ -50,15 +50,17 @@ app.post('/authorize', (req, res) => {
 app.post('/createUser', (req, res) => {
   if (req.body.hasOwnProperty('login') && req.body.hasOwnProperty('password')) {
     const hashPass = bcrypt.hashSync(req.body.password, 6);
-    const user = new User({ login: req.body.login, password: hashPass });
     User.findOne({ login: req.body.login }).then(result => {
       if (result) {
         res.status(404).send('this user already exists');
-      } else user.save().then(result => {
-        res.send({ data: result });
-      });
+      } else {
+        const user = new User({ login: req.body.login, password: hashPass });
+        user.save().then(result => {
+          res.send({ data: result });
+        });
+      }
     });
-  } else res.status(422).send('invalid property name');  
+  } else res.status(422).send('invalid property name');
 });
 
 app.listen(8000, () => {
